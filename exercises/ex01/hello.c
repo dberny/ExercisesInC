9d1cf7cb9adb9edb35ee3b919cc908fd236d4319
dberny <diego.berny@students.olin.edu>
2019-01-28 23:23:31 -0500

    testing done on hello.c regarding optimization

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int x = 5;
+	int y = x + 1; // when optimized using -O2, this does not actually add 1 to
+					// its value, but without it does print out 6
+    printf("%d\n", y);
     return 0;
 }
