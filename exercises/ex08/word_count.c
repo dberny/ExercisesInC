15baca6b20026efb84ac5f2864e952f3724b9a32
dberny <diego.berny@students.olin.edu>
2019-04-10 20:16:25 -0400

    better

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -19,29 +19,34 @@ int main() {
 	}
 
 	char str[MAXLENGTH];
-	// guint hashFunc = g_string_hash;
 	GHashTable *hashTable = g_hash_table_new(g_str_hash, g_str_equal);
-	while (fgets(str, MAXLENGTH, file) != NULL) {
-		// int val = g_hash_table_lookup(hashTable, str);
+	// while (fgets(str, MAXLENGTH, file) != NULL) {
+	while (fscanf(file, " %50s", str) == 1) {
 		if (g_hash_table_lookup(hashTable, str) == NULL) {
 			guint one = 1;
 			g_hash_table_insert(hashTable, g_strdup(str), &one);
 		}
 		else {
-			gpointer val = g_hash_table_lookup(hashTable, str);
-			int newVal = *(int *)val +1;
-			g_hash_table_replace(hashTable, str, &newVal);
+			gpointer oldvalue = g_hash_table_lookup(hashTable, str);
+			int newVal = *(int *)oldvalue +1;
+			// int *val;
+			// val = (int *) malloc(sizeof(int));
+			// *val = newVal;
+			g_hash_table_insert(hashTable, str, &newVal);
 		}
 	}
+	printf("finished scan\n");
 
 
 	GHashTableIter iter;
 	gpointer key, value;
 
 	g_hash_table_iter_init (&iter, hashTable);
+	printf("iter initialized\n");
 	while (g_hash_table_iter_next (&iter, &key, &value)) {
 	    	// do something with key and value
-			printf("key: %s, val: %i\n", (char *)key, *(int *)value);
+			printf("key: %s", (char *)key);
+			printf("key: %s, val: %i\n", (char *)key, **(int **)value);
 	}
 	return 0;
 }
